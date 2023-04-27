881d059cfda8eb8ffb36a153b78ce57adc67f280
Brian McClain <brianmmcclain@gmail.com>
2023-04-27 10:16:55 -0400

    Move resource_group resource to network.tf

--- main.tf
+++ main.tf
@@ -12,10 +12,3 @@ provider "azurerm" {
 }
 
 data "azurerm_client_config" "current" {}
-
-# A resource group is like a folder for related resources. You can delete the resource group to delete all resources in it.
-resource "azurerm_resource_group" "rg" {
-  name     = "terraform-tutorial"
-  location = "westus3"
-}
-
--- network.tf
+++ network.tf
@@ -1,3 +1,8 @@
+# A resource group is like a folder for related resources. You can delete the resource group to delete all resources in it.
+resource "azurerm_resource_group" "rg" {
+  name     = "terraform-tutorial"
+  location = "westus3"
+}
 
 # Create an virtual network and subnet
 resource "azurerm_virtual_network" "test" {
